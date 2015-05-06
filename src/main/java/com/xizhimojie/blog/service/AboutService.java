aa9842e4e686e3cbf5832254b0d0b181539a5c9a
john Xu <pronounce_john@yahoo.com>
2015-05-07 00:54:42 +0800

    merge 2.0

--- src/main/java/com/xizhimojie/blog/service/AboutService.java
+++ src/main/java/com/xizhimojie/blog/service/AboutService.java
@@ -10,25 +10,26 @@ import com.xizhimojie.blog.domain.About;
 import com.xizhimojie.common.persistence.DBUtilsHelper;
 
 public class AboutService {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(AboutService.class);
-	
+
 	private static final String SQL_GET_ABOUT = "SELECT ID, CONTENT FROM BLOG_ABOUT WHERE ID = 1";
-	
+
 	private static final String SQL_UPDATE_ABLUT = "UPDATE BLOG_ABOUT SET CONTENT = ? WHERE ID = 1";
-	
+
 	public About getAbout() {
 		QueryRunner runner = DBUtilsHelper.getRunner();
-		ResultSetHandler<About> resultSetHandler = new BeanHandler<About>(  
+		ResultSetHandler<About> resultSetHandler = new BeanHandler<About>(
 				About.class);
 		try {
 			return runner.query(SQL_GET_ABOUT, resultSetHandler);
 		} catch (Exception e) {
+
 			logger.error(e + "");
 		}
 		return null;
 	}
-	
+
 	public void updateAbout(String content) {
 		QueryRunner runner = DBUtilsHelper.getRunner();
 		try {
@@ -37,7 +38,5 @@ public class AboutService {
 			logger.error(e + "");
 		}
 	}
-	
-	
-			
+
 }
--- src/main/java/com/xizhimojie/blog/servlet/front/TagsForItemServlet.java
+++ src/main/java/com/xizhimojie/blog/servlet/front/TagsForItemServlet.java
@@ -8,9 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.StringUtils;
-
 import com.xizhimojie.blog.domain.Post;
 import com.xizhimojie.blog.domain.Tag;
 import com.xizhimojie.blog.service.PostService;
